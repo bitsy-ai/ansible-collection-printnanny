7aab5ad9df0ec774e0a3c91fe0f4c338bbf494e1
Leigh Johnson <leigh@bitsy.ai>
2021-11-19 21:36:08 -0800

    shellcheck

--- tools/bump-version.sh
+++ tools/bump-version.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
 
 FILES=("galaxy.yml")
-
-old_tag="$(echo $1 | sed 's/v//g')"
-new_tag="$(echo $2 | sed 's/v//g')"
+old_tag=${1:1}
+new_tag=${2:1}
 for f in "${FILES[@]}"
 do
    :
-   sed -i "s/$old_tag/$new_tag/g" $f
+   sed -i "s/$old_tag/$new_tag/g" "$f"
 done
 
 git add -A
 git config --global user.email "releases@bitsy.ai"
 git config --global user.name "Release Automation"
 git commit -m "🚀 Bump version: $1 -> $2"
-git push origin $3
+git push origin "$3"
