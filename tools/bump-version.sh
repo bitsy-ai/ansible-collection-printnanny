f927b0b727daffb38aaf3a60aaa0c36e762cf465
Leigh Johnson <hi@leighjohnson.me>
2021-11-19 17:41:05 -0800

    move bumpversion into script

--- tools/bump-version.sh
+++ tools/bump-version.sh
@@ -5,5 +5,10 @@ FILES=("galaxy.yml")
 for f in "${FILES[@]}"
 do
    : 
-   sed "s/$1/$2/g" $f
+   sed -i "s/$1/$2/g" $f
 done
+
+git add -A
+git config --global user.email "releases@bitsy.ai"
+git config --global user.name "Release Automation"
+git commit -m "🚀 Bump version: $1 -> $2"
